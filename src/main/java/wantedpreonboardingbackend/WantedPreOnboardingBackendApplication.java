2b2d1666d1c200dcc77c4d00b9a933ab88ccc57a
aaa <cnj991279@gmail.com>
2023-10-05 10:53:15 +0900

    Init: 기초 설정

--- src/main/java/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.java
+++ src/main/java/wantedpreonboardingbackend/WantedPreOnboardingBackendApplication.java
@@ -1,4 +1,4 @@
-package com.wantedpreonboardingbackend;
+package wantedpreonboardingbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
